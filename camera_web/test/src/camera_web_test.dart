9340438b7653bb9b00c4e728e56875f3a5d237ec
Martin Edlman <ac@an.y-co.de>
2021-08-18 22:56:48 +0200

    Use own loader widget (Lottie animation)
    Signed-off-by: Martin Edlman <ac@an.y-co.de>

--- camera_web/test/src/camera_web_test.dart
+++ camera_web/test/src/camera_web_test.dart
@@ -1,6 +1,6 @@
 @TestOn('chrome')
-
 import 'dart:html';
+
 import 'package:camera_platform_interface/camera_platform_interface.dart';
 import 'package:camera_web/camera_web.dart';
 import 'package:flutter/widgets.dart' show Widget;
@@ -36,7 +36,7 @@ void main() {
         () => mediaDevices.getUserMedia(any()),
       ).thenAnswer((_) async => videoElement.captureStream());
       CameraPlatform.instance = CameraPlugin()..window = window;
-      textureId = (await CameraPlatform.instance.create(const CameraOptions()));
+      textureId = await CameraPlatform.instance.create(const CameraOptions());
     });
 
     testWidgets('$CameraPlugin is the live instance', (tester) async {
